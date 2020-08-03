1538b73db171dfd13723643b2ebf2de6604ba391
avangersKinkyClient <63201809+avangersKinkyClient@users.noreply.github.com>
2020-08-03 09:44:50 +0200

    Update RuneScape.java
    Add Area code
    Add  public void onStart()
    Add public final String formatTime(final long ms)

--- RuneScape.java
+++ RuneScape.java
@@ -17,4 +17,21 @@ import java.awt.*;
 public class MiningBot extends AbstractScript {
     private long startTime;
     private int oreMined;
+
+    Area bankArea = new Area(3185, 3444, 3180, 3443, 0);
+    Area treeArea = new Area(3176, 3368, 3174, 3366, 0);
+
+    @Override
+    public void onStart() {
+        startTime = System.currentTimeMillis();
+        getSkillTracker().start();
+    }
+
+    public final String formatTime(final long ms) {
+        long s = ms / 1000, m = s / 60, h = m / 60;
+        s %= 60;
+        m %= 60;
+        h %= 24;
+        return String.format("%02d:%02d:%02d", h, m, s);
+    }
 }
